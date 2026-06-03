ba6c9db4391aef5db298bd13f4014e8535eee6ea
nehalrauf199-sudO <raufh137@gmail.com>
2026-06-03 12:18:08 +0500

    Add redirect for old IOSH slug URL

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/courses/iosh-managing-safely-version-5.0',
+                destination: '/courses/id/6a1eb863423312e3b7fc046d',
+                permanent: true,
+            },
+            // Add more redirects here for other old slug URLs
+        ];
+    },
+};
+
+module.exports = nextConfig;
